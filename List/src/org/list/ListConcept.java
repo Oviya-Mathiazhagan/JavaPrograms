5f924310f77c54dcb25bc1d55ad4975f53083086
Oviya-Mathiazhagan <146452966+Oviya-Mathiazhagan@users.noreply.github.com>
2024-09-24 20:29:50 -0800

    Add files via upload

---
+++ List/src/org/list/ListConcept.java
@@ -0,0 +1,37 @@
+package org.list;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListConcept {
+	public static void main(String[] args) {
+		List li = new ArrayList();
+		li.add(10);
+		li.add(true);
+		li.add('O');
+		li.add("oviya");
+		li.add(10);
+		
+		System.out.println(li);
+		
+	/*	int size = li.size();
+		System.out.println(size);
+		
+		int indexOf = li.indexOf("oviya");
+		System.out.println(indexOf);
+		
+		int lastIndexOf = li.lastIndexOf(10);
+		System.out.println(lastIndexOf);
+		
+		boolean contains = li.contains(true);
+		System.out.println(contains);
+		
+		Object object = li.get(4);
+		System.out.println(object); */
+		
+		for(int i = 0;i < li.size();i++) {
+			Object object = li.get(i);
+			System.out.println(object);
+		}
+	}
+
+}
